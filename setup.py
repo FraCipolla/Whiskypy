307e2211843fbbe24fe6a709f1a5d8ec69cee86b
FraCipolla <matcip@hotmail.com>
2025-02-17 11:53:45 +0100

    jwt import

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     # Needed to actually package something
     packages=['whiskypy'],
     # Needed for dependencies
-    install_requires=['requests'],
+    install_requires=['requests', 'jwt'],
     # *strongly* suggested for sharing
     version='0.2',
     # The license can be anything you like
