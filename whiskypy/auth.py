611d15a255c62b6ae60ad104bab11c349db1f6a9
FraCipolla <matcip@hotmail.com>
2025-02-17 17:04:49 +0100

    auth class test

--- whiskypy/auth.py
+++ whiskypy/auth.py
@@ -4,22 +4,21 @@ class ow_auth:
     def __init__(self, func):
         self._fn = func
 
-    def __call__(self, fn):
-        def wrapper(args, **kwargs):
-            token = args['__ow_headers'].get('authorization', False)
-            if not token:
-                return {'statusCode': 401, "body": {"error": "missing Authorization"}}
-            token_spl = token.split(' ')
-            if token_spl[0] != 'Bearer':
-                return {"statusCode": 401, "body": {"error": "Authorization failed"}}
-            secret = args['JWT_SECRET']
-            self.token = token_spl[1]
-            try:
-                self.decoded = jwt.decode(self.token, key=secret, algorithms='HS256')
-            except Exception as e:
-                return {"statusCode": 401, "body": {"error": e}}
-            return fn(args, **kwargs)
-        return wrapper
+    def __call__(self, *args, **kwargs):
+        print(args)
+        token = args['__ow_headers'].get('authorization', False)
+        if not token:
+            return {'statusCode': 401, "body": {"error": "missing Authorization"}}
+        token_spl = token.split(' ')
+        if token_spl[0] != 'Bearer':
+            return {"statusCode": 401, "body": {"error": "Authorization failed"}}
+        secret = args['JWT_SECRET']
+        self.token = token_spl[1]
+        try:
+            self.decoded = jwt.decode(self.token, key=secret, algorithms='HS256')
+        except Exception as e:
+            return {"statusCode": 401, "body": {"error": e}}
+        return self._fn(*args, **kwargs)
 
 def auth_decorator():
     def decorator(function):
